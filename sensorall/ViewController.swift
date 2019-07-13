e46d9a091ad8b5aaf9056b4a4e1fe0308108808b
Enjelina <enjel.hutasoit@users.noreply.github.com>
2019-07-13 14:29:53 +0700

    Initial Commit

---
+++ sensorall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sensorall
+//
+//  Created by Enjelina on 13/07/19.
+//  Copyright © 2019 enjelhutasoit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
